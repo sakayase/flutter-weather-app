b2f98fa2e262bcb81590d16137a9c531eb7c6834
sakayase <simon.ponitzki@gmail.com>
2023-04-11 23:39:41 +0200

    feat: ajout init getIt

--- lib/application/init.dart
+++ lib/application/init.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:weather_app/data/api/autocomplete.dart';
 import 'package:weather_app/data/api/weather.dart';
+import 'package:weather_app/domain/controllers/autocomplete.dart';
+import 'package:weather_app/domain/controllers/forecast.dart';
 import 'package:weather_app/domain/controllers/location.dart';
 import 'package:weather_app/domain/controllers/locale.dart';
+import 'package:weather_app/domain/controllers/weather.dart';
 
 GetIt getIt = GetIt.I;
 
@@ -12,7 +15,10 @@ init() {
   checkLocale();
   getIt.registerSingleton<LocationController>(LocationController());
   getIt.registerSingleton<WeatherAPI>(WeatherAPI());
+  getIt.registerSingleton<WeatherController>(WeatherController());
   getIt.registerSingleton<AutocompleteAPI>(AutocompleteAPI());
+  getIt.registerSingleton<AutocompleteController>(AutocompleteController());
+  getIt.registerSingleton<ForecastController>(ForecastController());
 }
 
 checkLocale() {
