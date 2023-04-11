941bee8f60c6271fe0b0a483e2b1d1513ef1668a
sakayase <simon.ponitzki@gmail.com>
2023-04-11 23:39:41 +0200

    feat: ajout diverses fonctions helper

--- lib/foundation/utils.dart
+++ lib/foundation/utils.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:weather_app/application/init.dart';
+import 'package:weather_app/data/models/icon_data.dart' as app_data;
 
 import 'package:weather_app/domain/controllers/locale.dart';
+import 'package:weather_app/foundation/settings.dart';
+
+DateTime dateTimeFromUnixTime(int unixTime) {
+  return DateTime.fromMillisecondsSinceEpoch(unixTime * 1000).toLocal();
+}
 
 Locale getLocale() {
   SystemLocale systemLocale = getIt.get<SystemLocale>();
@@ -13,8 +19,20 @@ String formatDate(DateTime date) {
   return DateFormat('dd/MM').format(date);
 }
 
-int daysBetween(DateTime from, DateTime to) {
-  from = DateTime(from.year, from.month, from.day);
-  to = DateTime(to.year, to.month, to.day);
-  return (to.difference(from).inHours / 24).round();
+String formatHour(DateTime date) {
+  return DateFormat('HH:mm').format(date);
+}
+
+bool intToBool(int int) {
+  return int != 0;
+}
+
+app_data.IconData getConditionIconFromCode(int code) {
+  return Settings.iconList.firstWhere((element) => element.code == code);
+}
+
+class NavigationView {
+  NavigationView({required this.view, required this.index});
+  Widget view;
+  int index;
 }
