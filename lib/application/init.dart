623cadeb9b6c3b8e3daa1f243d5aa6ddfa5e81fd
sakayase <simon.ponitzki@gmail.com>
2023-04-11 23:39:41 +0200

    feat: initialisation de GetIt

--- lib/application/init.dart
+++ lib/application/init.dart
@@ -1,1 +1,10 @@
-init() {}
+import 'package:get_it/get_it.dart';
+import 'package:weather_app/data/api/weather.dart';
+import 'package:weather_app/domain/location.dart';
+
+GetIt getIt = GetIt.I;
+
+init() {
+  getIt.registerSingleton<LocationFinder>(LocationFinder());
+  getIt.registerSingleton<WeatherAPI>(WeatherAPI());
+}
