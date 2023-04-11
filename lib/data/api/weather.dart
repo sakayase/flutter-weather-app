fcfd1af718cd640bed42ad434b32dd2e8733055c
sakayase <simon.ponitzki@gmail.com>
2023-04-11 23:39:41 +0200

    feat: Ajout de la locale dans la requete api

--- lib/data/api/weather.dart
+++ lib/data/api/weather.dart
@@ -1,16 +1,20 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_app/data/models/forecast.dart';
 import 'package:weather_app/data/models/location.dart';
 import 'package:weather_app/data/models/weather.dart';
 import 'package:weather_app/env.dart';
+import 'package:weather_app/foundation/utils.dart';
 
 class WeatherAPI {
+  Locale locale = getLocale();
+
   Future<Weather?> getCurrentWeather(Location location) async {
     String latLon = '${location.lat},${location.lon}';
     Uri url = Uri.parse(
-        '${env.apiUrl}/current.json?key=${env.weatherApiKey}&q=$latLon');
+        '${env.apiUrl}/current.json?key=${env.weatherApiKey}&q=$latLon&lang=${locale.languageCode}');
     try {
       var resp = await http.get(url);
       if (resp.statusCode == 200) {
@@ -26,7 +30,7 @@ class WeatherAPI {
   Future<Forecast?> getDaysForecast(Location location) async {
     String latLon = '${location.lat},${location.lon}';
     Uri url = Uri.parse(
-        '${env.apiUrl}/forecast.json?key=${env.weatherApiKey}&q=$latLon');
+        '${env.apiUrl}/forecast.json?key=${env.weatherApiKey}&q=$latLon&lang=${locale.languageCode}');
     try {
       var resp = await http.get(url);
       if (resp.statusCode == 200) {
