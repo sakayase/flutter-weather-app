b220174e3b64f34ad7cf4c4f3b1b5606e222be78
sakayase <simon.ponitzki@gmail.com>
2023-04-11 23:39:41 +0200

    feat: fonction de formattage de date fonction de calcul de jour entre deux dates

--- lib/foundation/utils.dart
+++ lib/foundation/utils.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/domain/controllers/locale.dart';
+import 'package:intl/intl.dart';
+import 'package:weather_app/application/init.dart';
 
-import '../application/init.dart';
+import 'package:weather_app/domain/controllers/locale.dart';
 
 Locale getLocale() {
   SystemLocale systemLocale = getIt.get<SystemLocale>();
   return systemLocale.locale;
+}
+
+String formatDate(DateTime date) {
+  return DateFormat('dd/MM').format(date);
+}
+
+int daysBetween(DateTime from, DateTime to) {
+  from = DateTime(from.year, from.month, from.day);
+  to = DateTime(to.year, to.month, to.day);
+  return (to.difference(from).inHours / 24).round();
 }
