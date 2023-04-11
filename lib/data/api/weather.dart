67cfdd00a857dba9a03b6c82165e42e2011360ee
sakayase <simon.ponitzki@gmail.com>
2023-04-11 23:39:41 +0200

    feat: modif api weather avec nb de days dans forecast

--- lib/data/api/weather.dart
+++ lib/data/api/weather.dart
@@ -6,6 +6,7 @@ import 'package:weather_app/data/models/forecast.dart';
 import 'package:weather_app/data/models/location.dart';
 import 'package:weather_app/data/models/weather.dart';
 import 'package:weather_app/env.dart';
+import 'package:weather_app/foundation/settings.dart';
 import 'package:weather_app/foundation/utils.dart';
 
 class WeatherAPI {
@@ -30,7 +31,7 @@ class WeatherAPI {
   Future<Forecast?> getDaysForecast(Location location) async {
     String latLon = '${location.lat},${location.lon}';
     Uri url = Uri.parse(
-        '${env.apiUrl}/forecast.json?key=${env.weatherApiKey}&q=$latLon&lang=${locale.languageCode}');
+        '${env.apiUrl}/forecast.json?key=${env.weatherApiKey}&q=$latLon&lang=${locale.languageCode}&days=${Settings.nbForecastDay}');
     try {
       var resp = await http.get(url);
       if (resp.statusCode == 200) {
